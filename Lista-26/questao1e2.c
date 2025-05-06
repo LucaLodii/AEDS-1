0433fd579f4193e894823d6bb906a301f58bbca2
Luca Guimarães Lodi <1560271@sga.pucminas.br>
2025-05-06 14:55:38 -0300

    Lista 26 até a 3 feita 4 incompleta

--- Lista-26/questao1e2.c
+++ Lista-26/questao1e2.c
@@ -1,7 +1,36 @@
-const int MAX_LIN = 2;
-const int MAX_COL = 4;
+#include <stdio.h>
+#include <stdbool.h>
 
-void 
+#define MAX_LIN 2  // Agora é uma constante REAL em tempo de compilação
+#define MAX_COL 4  // Antes tava const int MAX_LIN = 2; E const int MAX_COL = 4; Porém dava problema
+
+bool iguais(float matriz1[MAX_LIN][MAX_COL], float matriz2[MAX_LIN][MAX_COL]){
+    for(int i = 0; i < MAX_LIN; i++) {
+        for(int j = 0; j < MAX_COL; j++) {
+            if (matriz1[i][j] != matriz2[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+void leMatriz(float matriz[MAX_LIN][MAX_COL]) 
+{
+    printf("\nDigite os valores da matriz %dx%d:\n", MAX_LIN, MAX_COL);
+    for(int i = 0; i < MAX_LIN; i++) {
+        for(int j = 0; j < MAX_COL; j++) {
+            printf("Posicao [%d][%d]: ", i, j);
+            scanf("%f", &matriz[i][j]); 
+        }
+    }
+}
+
+void escreveMatriz(float matriz[MAX_LIN][MAX_COL]) {
+        for(int i = 0; i < MAX_LIN; i++) {
+        for(int j = 0; j < MAX_COL; j++) {
+            printf("\nPosicao [%d][%d] = %.1f ", i, j, matriz[i][j]);
+        }
+    }
+}
 
 int main()
 {
@@ -9,13 +38,13 @@ int main()
     leMatriz(M1);
     float M2[MAX_LIN][MAX_COL];
     leMatriz(M2);
-    printf(“\n\nEscrevendo a primeira Matriz :\n”);
+    printf("\n\nEscrevendo a primeira Matriz :\n");
     escreveMatriz(M1);
-    printf(“\n\nEscrevendo a segunda Matriz :\n”);
+    printf("\n\nEscrevendo a segunda Matriz :\n");
     escreveMatriz(M2);
     if (iguais(M1, M2))
-        printf(“\nMatrizes iguais !”);
+    printf("\nMatrizes iguais !");
     else
-        printf(“\nMatrizes diferentes !”);
+    printf("\nMatrizes diferentes !");
     return 0;
 }
---
+++ Lista-26/questao3.c
@@ -0,0 +1,55 @@
+// EU APAGUEI COISAS INUTEIS COMO A FUNCAO BOOL E A FUNCAO Q ESCREVE OQ EU ACABEI DE DIGITAR
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_LIN 2
+#define MAX_COL 4
+
+void escreveMatriz(float matriz[MAX_LIN][MAX_COL]) {
+        for(int i = 0; i < MAX_LIN; i++) {
+        for(int j = 0; j < MAX_COL; j++) {
+            printf("\nPosicao [%d][%d] = %.1f ", i, j, matriz[i][j]);
+        }
+    }
+}
+
+void copiarM1emM2(float matriz1[MAX_LIN][MAX_COL], float matriz2[MAX_LIN][MAX_COL]){    //Argumentos: os endereços para duas matrizes de reais
+    for(int i = 0; i < MAX_LIN; i++) {
+        for(int j = 0; j < MAX_COL; j++) {
+            matriz2[i][j] = matriz1[i][j]; 
+        }
+    }
+}
+
+void leMatriz(float matriz[MAX_LIN][MAX_COL])
+{
+    for (int i = 0; i < MAX_LIN; i++)
+    {
+        for (int j = 0; j < MAX_COL; j++)
+        {
+            printf("Posicao [%d][%d]: ", i, j);
+            scanf("%f", &matriz[i][j]);
+        }
+    }
+}
+
+int main()
+{
+    float M1[MAX_LIN][MAX_COL];
+    printf("\nDigite os valores da 1a matriz %dx%d:\n", MAX_LIN, MAX_COL);
+    leMatriz(M1);
+    float M2[MAX_LIN][MAX_COL];
+    printf("\nDigite os valores da 2a matriz %dx%d:\n", MAX_LIN, MAX_COL);
+    leMatriz(M2);
+
+    copiarM1emM2(M1, M2);
+
+    printf("\n\nEscrevendo a primeira Matriz :\n");
+    escreveMatriz(M1);
+    printf("\n\nEscrevendo a segunda Matriz :\n");
+    escreveMatriz(M2);
+
+    return 0;
+
+
+}
