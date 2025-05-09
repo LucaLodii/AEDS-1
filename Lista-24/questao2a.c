3b082e12eb68d77005b1e2892d078fe04d942888
Luca Guimarães Lodi <1560271@sga.pucminas.br>
2025-05-09 08:49:00 -0300

    Anotações Lista-28

--- Lista-24/questao2a.c
+++ Lista-24/questao2a.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
-// VERSÃO ITERATIVA
-void qtNegativo(float arr[], int size)
-{
-    int countNegativo = 0;
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] < 0)
-            countNegativo++;
+//VERSÃO ITERATIVA
+void qtNegativo(int arr[]){
+    int countNegativo;
+    for(int i = 1; i < 5; i++){
+        if(arr[i] < 0) countNegativo++;
     }
-    printf("\nA quantidade de numeros negativos no array eh: %i", countNegativo);
+    printf("\nA quantidade de numeros negativo eh: %i",countNegativo);
 }
 
-int main()
-{
-    float arr[5] = {-10, 5, -27, 4, -1};
-    int size = 5;
-    printf("\nNumeros presentes no array");
-    for (int i = 0; i < size; i++)
-    {
-        printf("\n%io: %.1f", i + 1, arr[i]);
-    }
-    qtNegativo(arr, size);
-
-    return 0;
+int main(){
+    int arr[5] = {10, -2, 21, 57, 1};
+    qtNegativo(arr);
 }
