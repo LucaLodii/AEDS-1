546052e8719fe305f91bfe3801d07f1c14383070
Luca Guimarães Lodi <luca.lodi.ll53@gmail.com>
2025-06-09 15:26:01 -0300

    data.h e data.cpp FEITOS

--- TPF/data/data.cpp
+++ TPF/data/data.cpp
@@ -4,66 +4,135 @@
 
 using namespace std;
 
-static const char* meses[] = {
-    "Janeiro", "Fevereiro", "Março", "Abril",
-    "Maio", "Junho", "Julho", "Agosto",
-    "Setembro", "Outubro", "Novembro", "Dezembro"
-};
+char *Data::mesExtenso()
+{
+    static char *extenso[] = {
+        (char *)"janeiro", (char *)"fevereiro", (char *)"marco", (char *)"abril", (char *)"maio", (char *)"junho",
+        (char *)"julho", (char *)"agosto", (char *)"setembro", (char *)"outubro", (char *)"novembro", (char *)"dezembro"};
 
-bool Data::dataValida() const {
-    if (ano < 1 || mes < 1 || mes > 12 || dia < 1) 
-        return false;
-    return dia <= diasMes();
+    if (dataValida())
+        return extenso[mes - 1];
+    else
+        return (char *)""; // Retorna string vazia se for inválido
 }
 
-const char* Data::mesExtenso() const {
-    return (mes >= 1 && mes <= 12)
-        ? meses[mes - 1]
-        : "Inválido";
+bool Data::dataValida()
+{
+    bool valida = true;
+    if (this->dia < 0 || this->dia > 31)
+        valida = false;
+    else if (this->mes < 0 || this->mes > 12)
+        valida = false;
+    return valida;
 }
 
-int Data::diasMes() const {
-    if (mes == 2) {
-        bool bissexto = (ano % 400 == 0) || (ano % 100 != 0 && ano % 4 == 0);
+int Data::diasMes() const
+{
+    if (mes == 2)
+    {
+        bool bissexto = false;
+        if (ano % 400 == 0)
+        {
+            bissexto = true;
+        }
+        else if (ano % 100 != 0 && ano % 4 == 0)
+        {
+            bissexto = true;
+        }
         return bissexto ? 29 : 28;
     }
-    static const int diasPorMes[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    static const int diasPorMes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     return diasPorMes[mes - 1];
 }
 
-void Data::escreveData() const {
+bool Data::setDia(int d)
+{
+    if (d >= 1 && d <= diasMes())
+    {
+        dia = d;
+        return true;
+    }
+    return false;
+}
+
+bool Data::setMes(int m)
+{
+    if (m >= 1 && m <= 12)
+    {
+        mes = m;
+        return true;
+    }
+    return false;
+}
+
+void Data::setAno(int a)
+{
+    ano = a;
+}
+
+int Data::getDia() const
+{
+    return dia;
+}
+
+int Data::getMes() const
+{
+    return mes;
+}
+
+int Data::getAno() const
+{
+    return ano;
+}
+
+void Data::escreveData() const
+{
     cout << dia << "/" << mes << "/" << ano;
 }
 
-void Data::leiaData() {
+void Data::leData()
+{
     int d, m, a;
-    do {
+    do
+    {
         cout << "Dia: ";
         cin >> d;
         cout << "Mês: ";
         cin >> m;
         cout << "Ano: ";
         cin >> a;
-    } while (!setDia(d) || !setMes(m));
-    setAno(a);
+        setAno(a); // Precisa vir antes do setDia para validar dias corretamente em fevereiro
+    } while (!setMes(m) || !setDia(d));
 }
 
-Data obterDataAtual() {
+Data obterDataAtual()
+{
     Data d;
     time_t now = time(nullptr);
-    tm* ltm = localtime(&now);
+    tm *ltm = localtime(&now);
     d.setAno(1900 + ltm->tm_year);
     d.setMes(1 + ltm->tm_mon);
     d.setDia(ltm->tm_mday);
     return d;
 }
 
-int calcularIdade(const Data& nascimento) {
+int calcularIdade(const Data &nascimento)
+{
     Data atual = obterDataAtual();
     int idade = atual.getAno() - nascimento.getAno();
-    if ((atual.getMes() < nascimento.getMes()) ||
-        (atual.getMes() == nascimento.getMes() && atual.getDia() < nascimento.getDia())) {
+
+    if (atual.getMes() < nascimento.getMes())
+    {
         idade--;
     }
+    else if (atual.getMes() == nascimento.getMes())
+    {
+        if (atual.getDia() < nascimento.getDia())
+        {
+            idade--;
+        }
+    }
+
     return idade;
 }
--- TPF/data/data.h
+++ TPF/data/data.h
@@ -1,50 +1,35 @@
 #ifndef DATA_H
 #define DATA_H
 
-#include "data.h" 
-#include <iostream>
-
-class Data {
+class Data
+{
 private:
     int dia;
     int mes;
     int ano;
 
 public:
-    // Setters
-    bool setDia(int d) {
-        if(d >= 1 && d <= 31) {
-            dia = d;
-            return true;
-        }
-        return false;
-    }
 
-    bool setMes(int m) {
-        if(m >= 1 && m <= 12) {
-            mes = m;
-            return true;
-        }
-        return false;
-    }
-
-    void setAno(int a) { ano = a; }
-
-    // Getters
-    int getDia() const { return dia; }
-    int getMes() const { return mes; }
-    int getAno() const { return ano; }
-
-    // Métodos
-    bool dataValida() const;
-    const char* mesExtenso() const;
     int diasMes() const;
+
+    bool setData(int dia, int mes, int ano);
+    Data getData();
+
+    bool setDia(int dia);
+    bool setMes(int mes);
+    void setAno(int ano);
+
+    int getDia() const;
+    int getMes() const;
+    int getAno() const;
+
+    char* mesExtenso();
+    bool dataValida();
+    void leData();
     void escreveData() const;
-    void leiaData();
 };
 
-// Declarações de funções livres
 Data obterDataAtual();
-int calcularIdade(const Data& nascimento);
+int calcularIdade(const Data &nascimento);
 
 #endif
